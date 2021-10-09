08e3a1a6ac2891644632c39ae0c6911168760410
JacobG03 <jacobgluszek03@gmail.com>
2021-10-09 12:03:01 +0200

    backend added

---
+++ api/app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_socketio import SocketIO
+from flask_cors import CORS
+from config import Config
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+socketio = SocketIO(app, cors_allowed_origins='*')
+CORS(app)
+
+
+from app import routes, sockets
