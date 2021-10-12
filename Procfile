a985b3ba103f5139f75927892d73a4bcd0424c2e
JacobG03 <jacobgluszek03@gmail.com>
2021-10-12 17:47:42 +0200

    changed folder structure for deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --pythonpath api main:app
+web: gunicorn main:app
--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from config import Config
 import os
 
 
-app = Flask(__name__, static_folder=os.path.abspath('../build'), static_url_path='/')
+app = Flask(__name__, static_folder=os.path.abspath('./frontend/build'), static_url_path='')
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
